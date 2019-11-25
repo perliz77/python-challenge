8555a748f0d1932a9f2e8c8f5f006154f4674ae4
Perla Lobera <perla.l.lobera@gmail.com>
2019-11-24 23:44:57 -0600

    Import os/csv. Add os.path.join. Add variables. Add total # of months. Add net total of Profit/Losses.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,28 @@
+#import OS with functions. Import csv module to read/write data in csv format:
 import os
 import csv
 
-csvfile = os.path.join("budget_data.csv")
-with open(csvfile, "r"):
-    csvread = csv.reader(csvfile)
-    
+#files needed for input and output:
+input_csvfile = os.path.join("Resources", "budget_data.csv")
+output_analysis = os.path.join("Analysis", "budget_analysis.txt")
 
+#variables needed:
+header = next
+total_months = 0
+total_net = 0
+# avg_changes_revenue =
+# greatest_increase =
+# greatest_decrease =
 
+#to open files:
+with open(input_csvfile) as data:
+    csvread = csv.reader(data)
+    #print(csvread)
+    header = next(csvread) 
+    #print(header)  
 
+#to find the total # of months included in dataset:   
+    for row in csvread:
+        total_months = total_months + 1
+#to find the net total amount and convert string into integer:
+        total_net = total_net + int(row[1])
