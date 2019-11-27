8f54d5d3b8ee3274f18501fd1fe55c4a86426e99
Perla Lobera <perla.l.lobera@gmail.com>
2019-11-27 17:39:25 -0600

    add average of the changes.

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,26 +10,37 @@ output_analysis = os.path.join("Analysis", "budget_analysis.txt")
 header = next
 total_months = 0
 total_net = 0
-# avg_changes_revenue =
+changes_total = 0
 # greatest_increase =
 # greatest_decrease =
 
 #to open files:
 with open(input_csvfile) as data:
     csvread = csv.reader(data)
-    #print(csvread)
+    
     header = next(csvread) 
-    #print(header)  
+    
 
 #to find the total # of months included in dataset:   
     for row in csvread:
         total_months = total_months + 1
+        
 #to find the net total amount and convert string into integer:
         total_net = total_net + int(row[1])
+        
 with open(input_csvfile, "r") as data:
 #to split data on delimiter (commas):
     csvread = csv.reader(data, delimiter=",")
+
 #to skip column headers:
     header = next(csvread)
+    
 #to start new list for cell values in 2nd column (#s):
     new_list = []
+
+#to find the average of the changes over the entire period:
+changes_total = sum(new_list)
+changes_avg = changes_total/total_months
+#print(changes_avg)
+
+
